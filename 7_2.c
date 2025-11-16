b5df04025fa04bc91eee033cc1e017d8be60d746
Yash Yadav <yashyadav4651@gmail.com>
2025-11-16 14:40:03 +0530

    Add files via upload

---
+++ 7_2.c
@@ -0,0 +1,30 @@
+// Write a C program to compute the monthly pay of 100 employees using each
+// employee‗s name, basic pay. The DA is computed as 52% of the basic pay.
+// Gross-salary (basic pay + DA). Print the employees name and gross salary
+#include <stdio.h>
+struct Employee // Structure to store employee details
+{
+    char name[50];
+    float basicPay;
+    float grossSalary;
+};
+int main()
+{
+    struct Employee emp[100]; // array for 100 employee
+    float DA, Gs;             // Gs=Gross salary
+    for (int i = 0; i < 100; i++)
+    {
+        printf("Enter Employee Name: \n");
+        scanf("%s", emp[i].name);
+        printf("Enter Basic Pay: \n");
+        scanf("%f", &emp[i].basicPay);
+
+        DA = 0.52 * emp[i].basicPay; // Calculate DA and basic Pay
+        Gs = emp[i].basicPay + DA;
+        printf("Name : %s\n", emp[i].name);
+        printf("Gross Salary: %.2f\n", Gs);
+        // Printf("Enter Gross Salary: ");
+        // scanf("%f", &Employee.grossSalary emp[i]);
+    }
+    return 0;
+}
---
+++ 7_4.c
@@ -0,0 +1,28 @@
+// Create a union containing 6 strings: name, home_address, hostel_address,
+// city, state and zip. Write a C program to display your present address.
+#include <stdio.h>
+union address
+{
+    char name[50];
+    char home_address[80];
+    char hostel_address[80];
+    char city[15];
+    char state[20];
+    int zip;
+
+} int main()
+{
+
+    printf("Enter Your Name: ");
+    scanf("%s", address.name);
+    printf("Enter Your Home Address: ");
+    scanf("%s", address.home_address);
+    printf("Enter Your Name: ");
+    scanf("%s", address.hostel_address);
+    printf("Enter Your Name: ");
+    scanf("%s", address.city);
+    printf("Enter Your Name: ");
+    scanf("%s", address.state);
+    printf("Enter Your Zip: ");
+    scanf("%d", address.zip);
+}
